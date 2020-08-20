b1b06f04b6377352ab21b7b6f1c601368f0f5c5c
charles66820 <charles.goedefroit@gmail.com>
2020-08-20 19:58:50 +0200

    darktheme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { AuthService } from './service/auth.service';
-import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {BreakpointObserver} from '@angular/cdk/layout';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'magicorp';
+  private _darkTheme = new Subject<boolean>();
+  isDarkTheme: Observable<boolean> = this._darkTheme.asObservable();
 
   constructor(
     public authService: AuthService,
     public breakpointObserver: BreakpointObserver
   ) { }
+
+  ngAfterViewInit(): void {
+    if (!localStorage.darkMode) this._darkTheme.next((window.matchMedia("(prefers-color-scheme: dark)")).matches);
+    else this._darkTheme.next(localStorage.darkMode == "true");
+  }
+
+  toggleDarkTheme(checked: boolean) {
+    localStorage.darkMode = checked;
+    this._darkTheme.next(checked);
+  }
 
 }
