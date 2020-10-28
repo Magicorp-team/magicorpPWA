26d84535deddff26def14f899058aba213c00a40
charles66820 <charles.goedefroit@gmail.com>
2020-10-28 18:42:07 +0100

    add last url redirection on signin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,22 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, AfterContentChecked } from '@angular/core';
 import { AuthService } from './service/auth.service';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { Observable, Subject } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, AfterContentChecked {
   title = 'magicorp';
   private _darkTheme = new Subject<boolean>();
   isDarkTheme: Observable<boolean> = this._darkTheme.asObservable();
+  lastUrl = '';
 
   constructor(
+    private route: ActivatedRoute,
     public authService: AuthService,
     public breakpointObserver: BreakpointObserver
   ) { }
@@ -26,6 +29,14 @@ export class AppComponent implements AfterViewInit {
   toggleDarkTheme(checked: boolean) {
     localStorage.darkMode = checked;
     this._darkTheme.next(checked);
+  }
+
+  ngAfterContentChecked() {
+    this.route.queryParams.subscribe(params => {
+      this.lastUrl = (params['lastUrl'] == undefined || params['lastUrl'] == '') ?
+        (location.pathname != '/signin' && location.pathname != '/signup') ?
+          location.href : params['lastUrl'] : params['lastUrl'];
+    });
   }
 
 }
